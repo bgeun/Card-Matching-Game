3c9e097fd613b9424bc0ce824dfc345ea5f99ed6
bgeun <papoke22@gmail.com>
2022-02-11 21:59:08 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,92 @@
+const clicks = document.querySelector(".move");
+const timer = document.querySelector(".time");
+const cards = document.querySelectorAll(".card");
+let firstCard;
+let secondCard;
+let hasFlipped = false;
+let lockboard = false;
+let move = 0;
+let pair = 0;
+let minute = 0;
+let seconds = 0;
+let interval;
+// -------------------------------------------------------------------
+
+function flipCard() {
+  move++;
+  clicks.innerHTML = move;
+  if (move == 1) {
+    startCounter();
+  }
+  if (lockboard) return;
+  if (firstCard === this) return;
+  this.classList.add("flip");
+
+  if (!hasFlipped) {
+    firstCard = this;
+    hasFlipped = true;
+    return;
+  }
+  secondCard = this;
+  lockboard = true;
+  isMatch();
+}
+function isMatch() {
+  let mach = firstCard.dataset.tech === secondCard.dataset.tech;
+  if (mach) {
+    disableCards();
+  } else {
+    unflipCard();
+  }
+}
+function disableCards() {
+  pair++;
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  if (pair === 6) {
+    endGame();
+  }
+  reseatBoard();
+}
+function unflipCard() {
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    reseatBoard();
+  }, 500);
+}
+function reseatBoard() {
+  firstCard = null;
+  secondCard = null;
+  hasFlipped = false;
+  lockboard = false;
+}
+
+(function shuffleCards() {
+  cards.forEach((card) => {
+    let postion = Math.floor(Math.random() * 12);
+    card.style.order = postion;
+  });
+})();
+function endGame() {
+  stopTimer();
+  alert(` 게임 끝! 클릭 ${move}번  ${minute}분 ${seconds}초`);
+}
+
+function startCounter() {
+  interval = setInterval(() => {
+    timer.innerHTML = minute + "min" + seconds + "sec";
+    seconds++;
+    if (seconds === 60) {
+      minute++;
+      seconds = 0;
+    }
+  }, 1000);
+}
+function stopTimer() {
+  clearInterval(interval);
+}
+
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
